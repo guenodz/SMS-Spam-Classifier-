b8437843bffba5df848d8bd815e826162b893529
Guendouz Mohamed <mohagueno@gmail.com>
2013-06-29 00:19:41 +0100

    First Commit.

---
+++ smsspam/src/main/java/com/guendouz/smsspam/Main.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright Guendouz Mohamed 2013
+ */
+package com.guendouz.smsspam;
+
+import com.guendouz.smsspam.classifiers.BaseClassifier;
+
+/**
+ * @author Guendouz Mohamed
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		/*
+		 * initialize the base classifier with the default classifier ( Naive
+		 * Bayes)
+		 */
+		BaseClassifier baseClassifier = new BaseClassifier();
+		baseClassifier.train();
+		System.err.println(baseClassifier
+				.classifyMessage("Where do you need to go to get it?"));
+
+	}
+
+}
---
+++ smsspam/src/main/java/com/guendouz/smsspam/classifiers/KnnClassifier.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright Guendouz Mohamed 2013
+ */
+package com.guendouz.smsspam.classifiers;
+
+import weka.classifiers.lazy.IBk;
+
+/**
+ * @author Guendouz Mohamed
+ * 
+ */
+public class KnnClassifier {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		/* initialize the Knn Classifier with k = 3 */
+		IBk knn = new IBk(3);
+		BaseClassifier classifier = new BaseClassifier();
+		classifier.setClassifier(knn);
+		classifier.train();
+		System.err.println(classifier
+				.classifyMessage("Where do you need to go to get it?"));
+
+	}
+
+}
